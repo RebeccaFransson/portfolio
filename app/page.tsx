65c205a29ea11ec4b64f51917ad9aaa3be030b64
rebeccafransson <ymafransson@gmail.com>
2024-08-25 13:36:47 +0200

    description text on home page

--- app/page.tsx
+++ app/page.tsx
@@ -8,24 +8,28 @@ export default function Home() {
           button left
         </div>
         <div className="h-full flex flex-col justify-between items-center">
-          <div className="flex flex-col gap-8 items-center">
-            <div className="">
-              <div className="relative flex gap-8 items-center p-4 bg-white rounded">
-                <h1 className=" font-bold text-7xl bg-gradient-to-l from-blue-950 to-black text-transparent bg-clip-text">
-                  Hello world, <br />
-                  I'm Rebecca!
-                </h1>
-                <Image
-                  className=" rounded"
-                  height="130"
-                  width="130"
-                  src="/profile.png"
-                  alt="a profile picture of rebecca"
-                />
+          <div className="flex flex-col gap-8 items-center pt-24">
+            <div className=" bg-white rounded p-4 max-w-[641px]">
+              <div className="flex flex-col gap-2 rounded bg-gradient-to-l from-blue-950 to-black text-transparent bg-clip-text">
+                <div className="flex gap-4 items-center">
+                  <h1 className=" font-extrabold text-7xl ">
+                    Hello world, <br />
+                    I'm Rebecca!
+                  </h1>
+                  <Image
+                    className="w-[130px] h-[130px] rounded"
+                    height="130"
+                    width="130"
+                    src="/profile.png"
+                    alt="a profile picture of rebecca"
+                  />
+                </div>
+
+                <p className="font-semibold text-xl">
+                  Results-driven and enthusiastic developer with a passion for
+                  creating features, building beautiful and reusable components.
+                </p>
               </div>
-              <p className="">
-                Frontend developer with over 7 years of experience
-              </p>
             </div>
           </div>
           <div className="h-[65px]">Wanna know more?</div>
